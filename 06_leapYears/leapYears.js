66c80232383ae47ed6f420014c551a1fae935d7a
Stéfano Troncaro <stefanotroncaro@gmail.com>
2024-07-14 19:16:32 -0300

    fix: solve exercise 05

--- 06_leapYears/leapYears.js
+++ 06_leapYears/leapYears.js
@@ -1,5 +1,9 @@
-const leapYears = function() {
+const leapYears = function(year) {
+  const divisibleBy4   = year % 4   === 0;
+  const divisibleBy100 = year % 100 === 0;
+  const divisibleBy400 = year % 400 === 0;
 
+  return divisibleBy4 && !(divisibleBy100 && !divisibleBy400);
 };
 
 // Do not edit below this line
