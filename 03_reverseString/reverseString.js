4be3596775144bf362495e32d5413599aea0f78d
Stéfano Troncaro <stefanotroncaro@gmail.com>
2024-07-14 18:10:20 -0300

    fix: solve exercise 03

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+  let reversed = '';
+  for (let i = 0; i < string.length; i++) {
+    reversed += string.at(-i-1);
+  }
+  return reversed;
 };
 
 // Do not edit below this line
