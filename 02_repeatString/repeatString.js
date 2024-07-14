747a970ea0809bb829a470cdb1321d4fb63becb4
Stéfano Troncaro <stefanotroncaro@gmail.com>
2024-07-14 18:04:54 -0300

    fix: solve exercise 02

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,14 @@
-const repeatString = function() {
+const repeatString = function(string, repetitions) {
+  if (repetitions < 0) {
+    return 'ERROR';
+  }
 
+  let output = '';
+  for (let i = 0; i < repetitions; i++) {
+    output += string;
+  }
+
+  return output;
 };
 
 // Do not edit below this line
