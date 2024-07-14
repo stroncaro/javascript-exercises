108b6ac9a4b6be9d1c505db804e9292e8388d3c9
Stéfano Troncaro <stefanotroncaro@gmail.com>
2024-07-14 19:25:28 -0300

    fix: solve exercise 07

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,11 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(fahrenheitDegrees) {
+  const celciusDegrees = (fahrenheitDegrees - 32) * 5/9;
+  return Math.round(celciusDegrees * 10) / 10;
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(celsiusDegrees) {
+  const fahrenheitDegrees = celsiusDegrees * 9/5 + 32;
+  return Math.round(fahrenheitDegrees * 10) / 10;
 };
 
 // Do not edit below this line
