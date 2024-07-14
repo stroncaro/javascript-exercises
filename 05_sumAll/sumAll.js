8f5ca601b23f149a54cd97095b2084b0fc173bdf
Stéfano Troncaro <stefanotroncaro@gmail.com>
2024-07-14 19:10:41 -0300

    fix: solve exercise 05

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,20 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+  if (typeof start !== 'number' || typeof end !== 'number') {
+    return 'ERROR'
+  }
 
+  if (start < 0 || end < 0) {
+    return 'ERROR'
+  }
+
+  const lo = start < end ? start : end;
+  const hi = start > end ? start : end;
+
+  let sum = 0;
+  for (let n = lo; n <= hi; n++) {
+    sum += n;
+  }
+  return sum;
 };
 
 // Do not edit below this line
